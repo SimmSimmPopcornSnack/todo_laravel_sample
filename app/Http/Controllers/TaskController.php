713e121225b25bef51d304b08afe1d129c6d3523
SimmSimmPopcornSnack <hiroto_o20@hotmail.com>
2022-01-03 09:06:11 +0900

    Done Chapter 4 Task list

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,17 +3,26 @@
 namespace App\Http\Controllers;
 
 use App\Models\Folder;
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
     public function index(int $id)
     {
+        // get info of all folders
         $folders = Folder::all();
+
+        // get a selected folder
+        $current_folder = Folder::find($id);
+
+        // get tasks attached to the folder
+        $tasks = $current_folder->tasks()->get();
 
         return view("tasks/index", [
             'folders' => $folders,
-            'current_folder_id' => $id,
+            'current_folder_id' => $current_folder->id,
+            'tasks' => $tasks,
         ]);
     }
 }
---
+++ app/Models/Task.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+
+class Task extends Model
+{
+    const STATUS = [
+        1 => ["label" => "未着手", "class" => "label-danger"],
+        2 => ["label" => "着手中", "class" => "label-info"],
+        3 => ["label" => "完了", "class" => ""],
+    ];
+    /**
+     * @return string
+     */
+    public function getStatusLabelAttribute()
+    {
+        $status = $this->attributes["status"];
+        // if not defined, return empty.
+        if(!isset(self::STATUS[$status])) {
+            return "";
+        }
+        return self::STATUS[$status]['label'];
+    }
+    /**
+     * @return string
+     */
+    public function getStatusClassAttribute() {
+        $status = $this->attributes["status"];
+        if(!isset(self::STATUS[$status])) {
+            return "";
+        }
+        return self::STATUS[$status]["class"];
+    }
+    /**
+     * @return string
+     */
+    public function getFormattedDueDateAttribute() {
+        return Carbon::createFromFormat("Y-m-d", $this->attributes["due_date"])
+            ->format("Y/m/d");
+    }
+}
